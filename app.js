2f705e12adb4a8414fc52e0de51d21d9c5d22092
Blake Gentry <blakesgentry@gmail.com>
2011-07-29 14:05:44 -0700

    Add session cookie. Allow listener port to take process.env.PORT

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ app.configure(function(){
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
+  app.use(express.cookieParser());
+  app.use(express.session({
+    secret: "21dae1be4774783b107b77cc30239e0d6a62ffb3573cb773ddf18398eba0622cc95db9f68f4d83216be3dddc5464b293ede9b62bfb4f8a388612caeab423c85e"
+  }));
 });
 
 app.configure('development', function(){
@@ -34,5 +38,7 @@ app.get('/', function(req, res){
   });
 });
 
-app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+var port = process.env.PORT || 3000
+app.listen(port, function(){
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
